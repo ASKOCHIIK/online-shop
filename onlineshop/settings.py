394e85180edf306e140753de6ed76cb524bd563e
askatkulmanov <askatkulmanov1@gmail.com>
2025-09-05 19:35:09 +0600

    ПРактикада ключтарды каттык

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -88,11 +88,11 @@ WSGI_APPLICATION = 'onlineshop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bootcamp_db',
-        'USER': 'bootcamp_32',
-        'PASSWORD': 'boot32',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
